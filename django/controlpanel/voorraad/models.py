e7224c2324584fb8c36941b9c0e22b237b251570
David Fokkema <davidf@nikhef.nl>
2008-07-28 12:17:00 +0200

    Tristan's voorraadbeheersysteem.

---
+++ django/controlpanel/voorraad/models.py
@@ -0,0 +1,206 @@
+from django.db import models
+from controlpanel.inforecords.models import *
+from django.contrib.auth.models import User
+from controlpanel.middleware import threadlocals
+from django.utils.safestring import mark_safe
+
+
+class Leverancier(models.Model):
+	naam = models.CharField(max_length=255)
+	adres = models.CharField(max_length=255)
+	postcode = models.CharField(max_length=255)
+	woonplaats = models.CharField(max_length=255)
+	provincie = models.CharField(blank=True,max_length=255)
+	land = models.CharField(max_length=255, default='Nederland')
+	telefoon = models.CharField(max_length=255)
+	fax = models.CharField(blank=True,max_length=255)
+	email = models.EmailField(blank=True)
+	website = models.URLField(blank=True)
+	contactpersoon = models.CharField(max_length=255)
+	opmerkingen = models.TextField(blank=True)
+
+	# standaard waarde
+	def __unicode__(self):
+		return self.naam
+
+        class Meta:
+                ordering = ['naam']
+                verbose_name_plural = 'Leveranciers'
+
+        class Admin:
+                list_display = ('naam','woonplaats','land','telefoon','contactpersoon')
+                list_filter = ('woonplaats','land')
+                search_fields = ('naam')
+                #ordering = ('naam')
+
+
+class Opbergplek(models.Model):
+	opbergplek = models.CharField(max_length=255,unique=True)
+
+        # standaard waarde
+        def __unicode__(self):
+                return self.opbergplek
+
+        class Meta:
+                ordering = ['opbergplek']
+                verbose_name_plural = 'Opbergplekken'
+
+        class Admin:
+                pass
+
+STATUS = (
+        ('V', 'V'),
+        ('?', '?'),
+        ('!', '!'),
+)
+
+class Artikel(models.Model):
+	naam = models.CharField(max_length=255)
+	opbergplek = models.ForeignKey(Opbergplek)
+	aantal = models.IntegerField()
+	leverancier = models.ForeignKey(Leverancier)
+	artikelnummer = models.CharField(max_length=255)
+	datum = models.DateTimeField(auto_now=True)
+        opmerkingen = models.TextField(blank=True)
+	status = models.CharField(max_length=1, core=True, choices=STATUS)
+
+        # standaard waarde
+        def __unicode__(self):
+                return self.naam
+
+	def commentaar(self):
+		if 'V' == self.status:
+			kleur = 'v'
+		elif '!' == self.status:
+			kleur = '<b>!</b>'
+		elif '?' == self.status:
+			kleur = '<b>?</b>'
+		return mark_safe(kleur)
+        commentaar.allow_tags = True
+	commentaar.short_description = "Status"
+
+        class Meta:
+                ordering = ['naam']
+		verbose_name_plural = 'Artikelen'
+
+        class Admin:
+                list_display = ('naam','opbergplek','aantal','leverancier','datum','commentaar')
+                list_filter = ('opbergplek','leverancier')
+                search_fields = ('naam')
+                #ordering = ('naam')
+
+class Apparatuur(models.Model):
+        naam = models.CharField(max_length=255)
+        opbergplek = models.ForeignKey(Opbergplek)
+        aantal = models.IntegerField()
+	opmerkingen = models.TextField(blank=True)
+
+        # standaard waarde
+        def __unicode__(self):
+                return self.naam
+
+        class Admin:
+                list_display = ('naam','opbergplek','aantal')
+                #list_filter = ('opbergplek')
+                search_fields = ('naam')
+
+	class Meta:
+                verbose_name_plural = 'Apparatuur'
+
+class Reservering(models.Model):
+	cluster = models.ForeignKey(Cluster)
+	aantal = models.IntegerField()
+	artikel = models.ForeignKey(Artikel)
+	datum = models.DateTimeField(auto_now_add=True)
+	voldaan = models.BooleanField(blank=True)
+        opmerkingen = models.TextField(blank=True)
+
+        # standaard waarde
+        def __unicode__(self):
+                return ('%s, %s, %s') % (self.cluster,self.datum, self.artikel)
+
+        class Meta:
+                ordering = ['datum','artikel']
+                verbose_name_plural = 'Reserveringen'
+
+        class Admin:
+                list_display = ('cluster','artikel','aantal','datum','voldaan')
+                list_filter = ('cluster','voldaan')
+                search_fields = ('artikel')
+                 
+
+class Gebruikt(models.Model):
+	persoon = models.ForeignKey(User)
+	artikel = models.ForeignKey(Artikel)
+	aantal = models.IntegerField()
+	datum = models.DateTimeField(auto_now_add=True)
+	cluster = models.ForeignKey(Cluster)
+#	reservering = models.ForeignKey(Reservering,blank=True)
+        opmerkingen = models.TextField(blank=True)
+
+# Om de voorraad actueel te houden
+        def save(self):
+                if self.id:
+                        oudewaarde = Gebruikt.objects.get(id=self.id).aantal
+			self.artikel.aantal = self.artikel.aantal-(self.aantal-oudewaarde)
+			self.artikel.save()
+			super(Gebruikt,self).save()
+                else:
+                        #self.persoon = threadlocals.get_current_user()
+                        super(Gebruikt,self).save()
+			self.artikel.aantal = self.artikel.aantal-self.aantal
+	                self.artikel.save()
+        # standaard waarde
+        def __unicode__(self):
+                return ('%s, %s') % (self.cluster, self.artikel)
+
+        class Meta:
+                ordering = ['datum','artikel']
+                verbose_name_plural = 'Gebruikt'
+
+        class Admin:
+                list_display = ('persoon','artikel','aantal','datum','cluster')
+                list_filter = ('persoon','cluster')
+                #search_fields = ('artikel')
+                #ordering = ('naam')
+
+
+class Bestel(models.Model):
+        persoon = models.ForeignKey(User,blank=True)
+        artikel = models.ForeignKey(Artikel)
+        aantalbesteld = models.IntegerField()
+        datumbesteld = models.DateTimeField(auto_now_add=True)
+	offerte = models.FileField(blank=True,upload_to='offerte')
+	levertijd = models.CharField(max_length=255,blank=True)
+        aantalgeleverd = models.IntegerField(default='0')
+        datumgeleverd = models.DateTimeField(auto_now=True)
+	verzendkosten =  models.CharField(blank=True,max_length=255)
+        voldaan = models.BooleanField(blank=True)
+        opmerkingen = models.TextField(blank=True)
+
+        def save(self):
+                if self.id:
+                        oudewaarde = Bestel.objects.get(id=self.id).aantalgeleverd
+                        self.artikel.aantal = self.artikel.aantal+(self.aantalgeleverd-oudewaarde)
+                        self.artikel.save()
+			if self.aantalgeleverd == self.aantalbesteld:
+				self.voldaan = True
+                        super(Bestel,self).save()
+                else:
+                        self.persoon = threadlocals.get_current_user()
+                        super(Bestel,self).save()
+
+        # standaard waarde
+        def __unicode__(self):
+                return ('%s %s') % (self.persoon, self.artikel)
+
+        class Meta:
+                ordering = ['datumbesteld','artikel']
+                verbose_name_plural = 'Bestellen'
+
+        class Admin:
+                list_display = ('persoon','artikel','aantalbesteld','datumbesteld','levertijd','aantalgeleverd','datumgeleverd','voldaan')
+                list_filter = ('persoon','voldaan')
+                search_fields = ('artikel')
+                #ordering = ('naam') s
+
