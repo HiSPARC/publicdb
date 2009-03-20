e92ce530bc71c120e90b9478475561192d09c26c
David Fokkema <davidf@nikhef.nl>
2009-03-20 11:32:34 +0100

    Added handling of the default student. When the client starts, the student name is empty and the gateway returns the first coincidence of 'TestStudent'. When the client hands in the results, these are ignored.

--- django_app/symposium2009/amfgateway.py
+++ django_app/symposium2009/amfgateway.py
@@ -11,17 +11,22 @@ def get_services(request):
 
 def get_coincidence(request, student_name):
     if not student_name:
-        return None, None
+        coincidence = AnalyzedCoincidence.objects.all()[0]
+    else:
+        try:
+            student = Student.objects.get(name=student_name)
+        except ObjectDoesNotExist:
+            student = Student(name=student_name)
+            student.save()
 
-    try:
-        student = Student.objects.get(name=student_name)
-    except ObjectDoesNotExist:
-        student = Student(name=student_name)
-        student.save()
-
-    coincidence = AnalyzedCoincidence.objects.filter(student=None)[0]
-    coincidence.student = student
-    coincidence.save()
+        try:
+            coincidence = AnalyzedCoincidence.objects.filter(
+                                student=student, is_analyzed=False)[0]
+        except IndexError:
+            coincidence = AnalyzedCoincidence.objects.filter(
+                                student=None)[0]
+            coincidence.student = student
+            coincidence.save()
 
     events = []
     for e in coincidence.coincidence.events.all():
@@ -30,23 +35,26 @@ def get_coincidence(request, student_name):
         # remove signals which are -999 (=> 569.43) baseline error
         signal = [x for x in signal if x < 500]
         signal = average(signal)
-        # dirty way to estimate muon equivalent
+        # dirty way to estimate muon equivalent, using the wished-for muon
+        # peak position
         signal /= -227
-        print e.detector, e.event_id, e.pulseheight1, e.pulseheight2, e.pulseheight3, e.pulseheight4
         event = {'latitude': e.latitude, 'longitude': e.longitude,
                  'signal': signal}
         events.append(event)
     return coincidence.pk, events
 
 def send_results(request, pk, core_position, log_energy, error_estimate):
-    print "Results have come in"
     coincidence = AnalyzedCoincidence.objects.get(pk=pk)
-    coincidence.core_position_x, coincidence.core_position_y = \
-        core_position
-    coincidence.log_energy = log_energy
-    coincidence.error_estimate = error_estimate
-    coincidence.is_analyzed = True
-    coincidence.save()
+
+    if coincidence.student.name == 'TestStudent':
+        return
+    else:
+        coincidence.core_position_x, coincidence.core_position_y = \
+            core_position
+        coincidence.log_energy = log_energy
+        coincidence.error_estimate = error_estimate
+        coincidence.is_analyzed = True
+        coincidence.save()
 
 
 services = { 'hisparc.get_services': get_services,
