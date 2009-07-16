a6d34b2fefffd2c0bf4df70e150b0c6f0e26d051
David Fokkema <davidf@nikhef.nl>
2009-07-16 13:55:48 +0200

    Added a function which Sam's new script uses to get a list of clusters, stations and location and status information.  Also, added a function we can use to log incoming AMF connections.  Will probably drop that very soon.

--- django_publicdb/histograms/amfgateway.py
+++ django_publicdb/histograms/amfgateway.py
@@ -1,11 +1,17 @@
 from pyamf.remoting.gateway.django import DjangoGateway
 from models import *
 from django.core.exceptions import ObjectDoesNotExist
+from django_publicdb.inforecords.models import *
 
 options_timehistogram = ['event time']
 options_1dhistogram = ['pulse heights', 'pulse integrals']
 
-def get_services(request):
+import logging
+logging.basicConfig(level=logging.INFO, filename='/tmp/test.log')
+logger = logging.getLogger('test')
+
+def get_services(request, arg1, arg2):
+    logger.info('%r, %r' % (arg1, arg2))
     return services.keys()
 
 def get_timehistogram_options(request):
@@ -59,11 +65,46 @@ def get_histogram(station_id, date, type):
     else:
         return histogram
 
+def get_stations(request):
+    """Get HiSPARC stations locations and status
 
-services = {'hisparc.get_services': get_services,
-            'hisparc.get_timehistogram_options': get_timehistogram_options,
-            'hisparc.get_timehistogram': get_timehistogram,
-            'hisparc.get_1dhistogram_options': get_1dhistogram_options,
-            'hisparc.get_1dhistogram': get_1dhistogram}
+    This function returns an array of dictionaries containing clusters,
+    stations and their coordinates as well as status information
+
+    """
+    data = []
+    for cluster in Cluster.objects.filter(country__icontains='netherlands'):
+        c = {}
+        c['name'] = cluster.name
+        c['status'] = 1.0
+        c['contents'] = []
+        for station in Station.objects.filter(location__cluster=cluster):
+            s = {}
+            try:
+                detector = DetectorHisparc.objects.get(station=station)
+            except ObjectDoesNotExist:
+                continue
+            s['number'] = station.number
+            s['latitude'] = detector.latitude
+            if not s['latitude']:
+                s['latitude'] = 52.0
+            s['longitude'] = detector.longitude
+            if not s['longitude']:
+                s['longitude'] = 4.0
+            s['status'] = 1.0
+            c['contents'].append(s)
+        data.append(c)
+
+    return data
+
+
+services = {
+    'hisparc.get_services': get_services,
+    'hisparc.get_timehistogram_options': get_timehistogram_options,
+    'hisparc.get_timehistogram': get_timehistogram,
+    'hisparc.get_1dhistogram_options': get_1dhistogram_options,
+    'hisparc.get_1dhistogram': get_1dhistogram,
+    'hisparc.get_stations': get_stations,
+}
 
 publicgateway = DjangoGateway(services)
