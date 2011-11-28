bf0d47c91a36609086ae3d8e30bfcbafa02874ce
David Fokkema <davidf@nikhef.nl>
2011-11-28 15:55:37 +0100

    Fix: only return stations which actually exist in group

--- django_publicdb/analysissessions/models.py
+++ django_publicdb/analysissessions/models.py
@@ -144,13 +144,19 @@ class SessionRequest(models.Model):
         main_cluster = self.cluster.main_cluster()
         cluster_group_name = '/hisparc/cluster_' + main_cluster.lower()
 
+        try:
+            cluster_group = data.getNode(cluster_group_name)
+        except tables.NodeError:
+            return []
+
         stations = []
         for station in Station.objects.filter(cluster=self.cluster):
             station_group_name = 'station_%d' % station.number
-            station_group = data.getNode(cluster_group_name,
-                                         station_group_name)
-            stations.append(station_group)
 
+            if station_group_name in cluster_group:
+                station_group = data.getNode(cluster_group_name,
+                                             station_group_name)
+                stations.append(station_group)
         return stations
   
    def save_coincidence(self,event_list,session):
