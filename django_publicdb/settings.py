99ed4aebc053a04f61171d6e8062dfdb09250a52
David Fokkema <davidf@nikhef.nl>
2012-07-03 15:14:37 +0200

    New Django 1.2 database settings

--- django_publicdb/settings.py
+++ django_publicdb/settings.py
@@ -9,13 +9,17 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'      # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 # DF: relative path, just for running test server!
-DATABASE_NAME = '../public.db'     # Or path to database file if using sqlite3.
-DATABASE_USER = ''      # Not used with sqlite3.
-DATABASE_PASSWORD = ''  # Not used with sqlite3.
-DATABASE_HOST = ''    # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+        'NAME': '../public.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
 
 # Path of the mounted HiSPARC datastore root folder
 # DEV_ONLY
