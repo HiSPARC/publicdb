4cd60ca6a9a239b3a2af4926e9d4f86d8afccb8b
David Fokkema <davidf@nikhef.nl>
2012-07-05 14:33:50 +0200

    Prepare for Django 1.4

--- django_publicdb/manage.py
+++ django_publicdb/manage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from django.core.management import execute_manager
+from django.core.management import execute_from_command_line
 try:
     import settings # Assumed to be in the same directory.
 except ImportError:
@@ -8,4 +8,4 @@ except ImportError:
     sys.exit(1)
 
 if __name__ == "__main__":
-    execute_manager(settings)
+    execute_from_command_line(settings)
