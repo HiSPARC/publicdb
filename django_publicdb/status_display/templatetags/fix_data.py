36cc73cd01f68f1789cf89b412ace8ddb9b7ea68
Arne de Laat <adelaat@nikhef.nl>
2012-07-12 11:05:56 +0200

    Add filter to round data to 2 decimal places
    Round data (temperature/barometer) to 2 decimal places
    More precision is unnecessary for the plots (saves bandwidth)
    Moreover, the data doesn't make smaller steps

--- django_publicdb/status_display/templatetags/fix_data.py
+++ django_publicdb/status_display/templatetags/fix_data.py
@@ -5,12 +5,14 @@ register = template.Library()
 
 @register.filter
 def fix_histogram_data(value):
-    """append one value to end of data, to fix histogram"""
+    """Append one value to end of data, to fix step histogram"""
 
     return value + [[value[-1][0] + (value[-1][0] - value[-2][0]), value[-1][1]]]
 
 @register.filter
 def fix_timestamps_in_data(values):
+    """Convert timestamps to hour of day"""
+
     x, y = zip(*values)
     seconds_in_day = [u % 86400 for u in x]
     hours_in_day = [u / 3600. for u in seconds_in_day]
@@ -19,4 +21,12 @@ def fix_timestamps_in_data(values):
 
 @register.filter
 def slice_data(values, arg):
+    """Get every nth value from the list"""
+
     return values[::arg]
+
+@register.filter
+def round_data(values, arg):
+    """Round every value to nth decimal place"""
+
+    return [[round(x, arg), round(y, arg)] for x, y, in values]
