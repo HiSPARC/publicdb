e6a10d6c0eef227c4572c92a4a49a46fa801a097
cblam <cblam@gmx.net>
2012-07-24 16:19:59 +0200

    Added tests for jsparc

--- django_publicdb/jsparc/tests.py
+++ django_publicdb/jsparc/tests.py
@@ -1,23 +1,191 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
+import re
+import time
+import string
+import sys
 
-Replace these with more appropriate tests for your application.
-"""
+import json
+import urllib
 
-from django.test import TestCase
+from django.core import mail
+from django.test import LiveServerTestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from django_publicdb.analysissessions.models import *
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+def is_json(response):
 
->>> 1 + 1 == 2
-True
-"""}
+    for left, right in [
+        (response.getcode(),            200),
+        (response.info().getmaintype(), "application"),
+        (response.info().getsubtype(),  "json")
+    ]:
+        if left != right:
+            return False
+
+    return True
+
+#-------------------------------------------------------------------------------
+# Client class for interacting with the server
+#-------------------------------------------------------------------------------
+
+class Client:
+
+    def receive_coincidence(
+        self,
+        session_title,
+        session_pin,
+        student_name
+    ):
+
+        #--------------------------------------
+        # Request data for a single coincidence
+
+        params = urllib.urlencode( {
+            "session_title" : session_title,
+            "session_pin"   : session_pin,
+            "student_name"  : student_name
+        } )
+
+        response = urllib.urlopen(
+            '%s%s?%s' % (
+                self.testcase.live_server_url,
+                '/jsparc/get_coincidence/',
+                params
+            )
+        )
+
+        return response
+
+
+    def send_result(
+        self,
+        session_title,
+        student_name,
+        data
+    ):
+
+        #-------------------------
+        # Push data back to jSparc
+
+        params = urllib.urlencode( {
+            "session_title" : session_title,
+            "student_name"  : student_name,
+            "pk"            : data[ 'pk' ],
+            "lat"           : data[ 'events' ][ 0 ][ 'lat' ],
+            "lon"           : data[ 'events' ][ 0 ][ 'lon' ],
+            "logEnergy"     : 15,
+            "error"         : 1
+        } )
+
+        response = urllib.urlopen(
+            '%s%s?%s' % (
+                self.testcase.live_server_url,
+                '/jsparc/result/',
+                params
+            )
+        )
+
+        return response
+
+
+class MyJsparcTests(LiveServerTestCase):
+
+    fixtures = [
+        'tests_inforecords',
+        'tests_analysissessions',
+        'tests_coincidences'
+    ]
+
+    #---------------------------------------------------------------------------
+    # Setup and teardown
+    #---------------------------------------------------------------------------
+
+    def setUp( self ):
+        super( MyJsparcTests, self ).setUp()
+
+    def tearDown( self ):
+        super( MyJsparcTests, self ).tearDown()
+
+    #---------------------------------------------------------------------------
+    # Tests
+    #---------------------------------------------------------------------------
+
+    def test_everythingCorrect( self ):
+        client = Client()
+
+        session = AnalysisSession.objects.all()[0]
+
+        # 1.
+
+        response = client.receive_coincidence(
+            session.title,
+            session.pin,
+            "Student 1"
+        )
+
+        assert(is_json(response))
+
+        # 2.
+
+        response = client.send_result(
+            session.title,
+            "Student 1",
+            json.loads( response.read() )
+        )
+
+        assert(is_json(response))
+
+        data = json.loads( response.read() )
+
+        self.assertEqual( data[ 'msg' ], "OK [result stored]" )
+
+    def test_wrongPin( self ):
+        client = Client()
+
+        session = AnalysisSession.objects.all()[0]
+
+        response = client.receive_coincidence(
+            session.title,
+            0000,
+            "Student 1"
+        )
+
+        assert(not is_json(response))
+
+    def test_wrongTitle( self ):
+        client = Client()
+
+        session = AnalysisSession.objects.all()[0]
+
+        response = client.receive_coincidence(
+            "Such a wrong title.. I guess!! l33t h4x0r!",
+            session.pin,
+            "Student 1"
+        )
+
+        assert(not is_json(response))
+
+    def test_wrongStudentName( self ):
+        client = Client()
+
+        session = AnalysisSession.objects.all()[0]
+
+        # 1.
+
+        response = client.receive_coincidence(
+            session.title,
+            session.pin,
+            "Student 1"
+        )
+
+        assert(is_json(response))
+
+        # 2.
+
+        response = client.send_result(
+            session.title,
+            "Student 2",
+            json.loads( response.read() )
+        )
+
+        assert(not is_json(response))
 
