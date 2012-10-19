4844f3f23cac4f3fcb023c9c738b2eee28dcde7f
Arne de Laat <adelaat@nikhef.nl>
2012-10-19 17:04:41 +0200

    Fix javascripts for IE 7 and 8
    Remove trailing ',' in objects (i.e. a ',' before a '}')
    Fix a for loop in flot, IE would go to far
    
    Add minified version of OpenLayers
    Add unminified version of flot

--- django_publicdb/status_display/static/scripts/flot_settings.js
+++ django_publicdb/status_display/static/scripts/flot_settings.js
@@ -6,32 +6,32 @@ var base_options = {
     series: {
         lines: {
             lineWidth: 1,
-            steps: true,},
-        shadowSize: 0,},
+            steps: true},
+        shadowSize: 0},
     yaxis: {
         show: true,
         position: "left",
         color: "#000",
         tickColor: "#000",
         font: {
-            size: 12,},
+            size: 12},
         tickLength: 4,
         tickDecimals: 0,
         labelWidth: 33,
         axisLabelUseCanvas: true,
-        axisLabelFontSizePixels: 16,},
+        axisLabelFontSizePixels: 16},
     xaxis: {
         show: true,
         position: "bottom",
         color: "#000",
         tickColor: "#000",
         font: {
-            size: 12,},
+            size: 12},
         tickLength: 4,
         tickDecimals: 0,
         labelHeight: 23,
         axisLabelUseCanvas: true,
-        axisLabelFontSizePixels: 16,},
+        axisLabelFontSizePixels: 16},
     y2axis: {
         show: true,
         position: "right",
@@ -39,7 +39,7 @@ var base_options = {
         alignTicksWithAxis: 1,
         axisLabel: '',
         labelWidth: 11,
-        tickFormatter: HideTickLabels,},
+        tickFormatter: HideTickLabels},
     x2axis: {
         show: true,
         position: "top",
@@ -47,7 +47,7 @@ var base_options = {
         alignTicksWithAxis: 1,
         axisLabel: '',
         labelHeight: 0,
-        tickFormatter: HideTickLabels,},
+        tickFormatter: HideTickLabels},
     grid: {
         show: true,
         aboveData: 0,
@@ -59,22 +59,22 @@ var base_options = {
         minBorderMargin: 0,
         clickable: false,
         hoverable: false,
-        autoHighlight: false,},
-    legend: {show: false,},
+        autoHighlight: false},
+    legend: {show: false}
 };
 
 var _eh_options = {
     series: {
         lines: {
-            lineWidth: 1.5,},},
+            lineWidth: 1.5}},
     yaxis: {
         axisLabel: 'Number of events',
-        min: 0,},
+        min: 0},
     xaxis: {
         axisLabel: 'Hour of day (UTC)',
         ticks: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24],
         min: 0,
-        max: 24,},
+        max: 24}
 };
 
 var _ph_options = {
@@ -83,9 +83,9 @@ var _ph_options = {
         min: 0.7,
         ticks: [1, 5, 10, 50, 100, 500, 1000, 5000, 10000],
         transform: function (N) {return Math.log(N);},
-        inverseTransform: function (N) {return Math.exp(N);},},
+        inverseTransform: function (N) {return Math.exp(N);}},
     xaxis: {
-        axisLabel: 'Pulseheight (mV)',},
+        axisLabel: 'Pulseheight (mV)'}
 };
 
 var _pi_options = {
@@ -94,41 +94,41 @@ var _pi_options = {
         ticks: [1, 5, 10, 50, 100, 500, 1000, 5000, 10000],
         min: 0.7,
         transform: function (N) {return Math.log(N);},
-        inverseTransform: function (N) {return Math.exp(N);},},
+        inverseTransform: function (N) {return Math.exp(N);}},
     xaxis: {
-        axisLabel: 'Pulseintegral (mVns)',},
+        axisLabel: 'Pulseintegral (mVns)'}
 };
 
 var _td_options = {
     series: {
         lines: {
-            show: false,},
+            show: false},
         points: {
             show: true,
-            radius: 0.1,},},
+            radius: 0.1}},
     yaxis: {
-        axisLabel: 'Temperature (°C)',},
+        axisLabel: 'Temperature (°C)'},
     xaxis: {
         axisLabel: 'Hour of day (UTC)',
         ticks: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24],
         min: 0,
-        max: 24,},
+        max: 24}
 };
 
 var _bd_options = {
     series: {
         lines: {
-            show: false,},
+            show: false},
         points: {
             show: true,
-            radius: 0.1,},},
+            radius: 0.1}},
     yaxis: {
-        axisLabel: 'Air pressure (hPa)',},
+        axisLabel: 'Air pressure (hPa)'},
     xaxis: {
         axisLabel: 'Hour of day (UTC)',
         ticks: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24],
         min: 0,
-        max: 24,},
+        max: 24}
 };
 
 var eh_options = $.extend(true, {}, base_options, _eh_options);
