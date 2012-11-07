976795b8775bb9e2d8f29b8ff8b5e2bc6358ee9c
Arne de Laat <adelaat@nikhef.nl>
2012-11-07 10:19:03 +0100

    Fix integral calculation in example script

--- examples/load_coincidence_data.py
+++ examples/load_coincidence_data.py
@@ -33,9 +33,9 @@ def save_coincidence(event_list):
         date_time = datetime.datetime.utcfromtimestamp(event['timestamp'])
         timestamps.append((date_time, event['nanoseconds']))
 
-        pulseheights = [x * .57 if x != -999 else -999 for x in
+        pulseheights = [x * .57 if x not in [-999, -1] else x for x in
                         event['pulseheights']]
-        integrals = [x * .57 if x != -999 else -999 for x in
+        integrals = [x * .57 * 2.5 if x not in [-999, -1] else x for x in
                      event['integrals']]
 
         dt = analyze_traces(traces)
