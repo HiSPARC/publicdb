ba713da3667fecd61f6b60dd90ca8f1b2545ec62
Arne de Laat <adelaat@nikhef.nl>
2012-11-15 12:39:00 +0100

    Specific csrf protection to fix data download

--- django_publicdb/settings.py
+++ django_publicdb/settings.py
@@ -95,7 +95,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.gzip.GZipMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django_publicdb.middleware.threadlocals.ThreadLocals',
