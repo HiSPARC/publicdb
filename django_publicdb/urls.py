f03df68da7b74082bb856cd421868dc26432ff95
Arne de Laat <adelaat@nikhef.nl>
2013-02-26 16:40:55 +0100

    Add a robots.txt
    This will block the robots from some pages
    Prevent bots from all the data pages
    Also block bots from the source files (csv)

--- django_publicdb/urls.py
+++ django_publicdb/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from django.views.generic import RedirectView
+from django.views.generic.simple import direct_to_template
 
 from django_publicdb.inforecords.views import *
 
@@ -8,10 +9,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^django_publicdb/', include('django_publicdb.foo.urls')),
-
     (r'^$', RedirectView.as_view(url='show/stations', permanent=False)),
+
+    (r'^robots\.txt$', direct_to_template, {'template': 'robots.txt', 'mimetype': 'text/plain'}),
 
     (r'^api/', include('django_publicdb.api.urls')),
     (r'^show/', include('django_publicdb.status_display.urls')),
@@ -23,6 +23,7 @@ urlpatterns = patterns('',
     (r'^config/datastore$', create_datastore_config),
 
     (r'^keys/(?P<host>\w+)/$', 'django_publicdb.inforecords.views.keys'),
+
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
