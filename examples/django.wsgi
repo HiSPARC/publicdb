f263603f4f77bf4497c35f09de1730f02a5d25b3
Mark de Neut Brossois <mark.dnb@live.nl>
2013-04-09 15:15:17 +0200

    edited vagrant files so it will work for publicdb

--- examples/django.wsgi
+++ examples/django.wsgi
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.append('/user/admhispa/http/pique')
+sys.path.append('/var/www')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'django_publicdb.settings'
 
 import django.core.handlers.wsgi
--- vagrant/puppet/modules/apache/manifests/init.pp
+++ vagrant/puppet/modules/apache/manifests/init.pp
@@ -2,15 +2,27 @@ class apache {
   package { "apache2":
     ensure => present,
   }
+  
+  package { "libapache2-mod-wsgi":
+    ensure => present,
+    require => Package["apache2"]
+  }
 
   service { "apache2":
     ensure => running,
     require => Package["apache2"],
   }
 
-  file { '/var/www':
+  file { "/var/www":
     ensure => link,
-    target => "/vagrant",
+    target => "/vagrant/django_publicdb",
+    notify => Service['apache2'],
+    force  => true
+  }
+ 
+  file { "/etc/apache2/httpd.conf":
+    ensure => link,
+    target => "/vagrant/vagrant_hisparc/puppet/modules/apache/manifests/hisparc.conf",
     notify => Service['apache2'],
     force  => true
   }
