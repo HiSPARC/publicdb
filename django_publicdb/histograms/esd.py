d4c92bba0d560c266877cb74bdff33318f253354
David Fokkema <davidf@nikhef.nl>
2013-05-07 14:12:29 +0200

    ESD code now runs, does nothing

--- django_publicdb/histograms/esd.py
+++ django_publicdb/histograms/esd.py
@@ -1,37 +1,48 @@
-""" Functions that do the processing of events
-    from the datastore to the ESD
-
-"""
+"""Process events from datastore and save Event Summary Data (ESD)"""
 import tempfile
 
 import tables
-from sapphire import process_events
+from sapphire.analysis import process_events
 
 import datastore
 import esd_storage
 
 
 def process_events_and_store_esd(summary):
+    """Process events from datastore and save Event Summary Data
 
+    :param summary: summary of data source (station and date)
+    :type summary: histograms.models.Summary instance
+
+    """
     date = summary.date
-    cluster = summary.station.cluster.main_cluster()
-    station_id = summary.station.number
+    station = summary.station
 
-    source_node = get_node_from_datastore(date, cluster, station_id)
+    filepath = datastore.get_data_path(date)
+    with tables.openFile(filepath, 'r') as data:
+        source_node = get_station_node_from_datastore_file(data, station)
+        print source_node
+
     #copy to tmp?
-    ... process_events(date, cluster, station_id)
+    #... process_events(date, cluster, station_id)
     #copy from tmp to esd
-    copy_node_from_tmp_to_esd(date, cluster, station_id)
-
-    pass
+    #copy_node_from_tmp_to_esd(date, cluster, station_id)
 
 
-def get_node_from_datastore(date, cluster, station_id):
-    # get events and blobs from datastore
-    datapath = get_data_path(date)
-    with tables.openFile(datapath, 'r') as file:
-        table = file.getNode('/hisparc/cluster_%s/station_%d/' %
-                             (cluster.lower(), station_id))
+def get_station_node_from_datastore_file(data, station):
+    """Return station node in datastore file
+
+    :param data: datastore file
+    :param station: inforecords.models.Station object
+
+    """
+    cluster = station.cluster.main_cluster()
+    station_id = station.number
+
+    group = data.getNode('/hisparc/cluster_%s/station_%d/' %
+                         (cluster.lower(), station_id))
+    return group
+
 
 def get_tmp_file(date, station_id):
     # create tmp file
@@ -43,6 +54,7 @@ def get_tmp_file(date, station_id):
 
 
 def copy_node_from_tmp_to_esd(tmp_node, esd_file):
+    pass
     # copy node from tmp to esd
 
 #     station_node = ...(tmp_node, station_id)
