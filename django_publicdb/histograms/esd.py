f608e3eefc847d8f229c9c96a6ecf7be31fce0a4
David Fokkema <davidf@nikhef.nl>
2013-05-08 15:35:24 +0200

    ESD now uses correct temporary files

--- django_publicdb/histograms/esd.py
+++ django_publicdb/histograms/esd.py
@@ -95,18 +95,14 @@ def process_events_and_store_esd(summary):
     filepath = datastore.get_data_path(date)
     with tables.openFile(filepath, 'r') as data:
         source_node = get_station_node(data, station)
-        # FIXME: JUST FOR TESTING
-        with tables.openFile('/tmp/test.h5', 'w') as f:
+        tmp_filename = create_temporary_file()
+        with tables.openFile(tmp_filename, 'w') as f:
             process_events = ProcessEventsFromSource(data, f, source_node,
                                                      '/')
             process_events.process_and_store_results(limit=100)
             copy_node_to_esd_file_for_summary(summary,
                                               process_events.destination)
-
-    #copy to tmp?
-    #... process_events(date, cluster, station_id)
-    #copy from tmp to esd
-    #copy_node_from_tmp_to_esd(date, cluster, station_id)
+    os.remove(tmp_filename)
 
 
 def get_or_create_station_node(data, station):
@@ -145,18 +141,21 @@ def get_station_node_path(station):
                                                station_id)
 
 
+def create_temporary_file():
+    """Create a temporary file and return the pathname
+
+    The file is closed as soon as it is created, to allow PyTables to open
+    it for writing
+
+    """
+    f = tempfile.NamedTemporaryFile(delete=False)
+    f.close()
+    return f.name
+
+
 def copy_node_to_esd_file_for_summary(summary, source_node):
     esd_path = esd_storage.get_or_create_esd_data_path(summary.date)
 
     with tables.openFile(esd_path, 'a') as esd_data:
         esd_group = get_or_create_station_node(esd_data, summary.station)
         source_node.copy(esd_group, createparents=True, overwrite=True)
-
-
-def get_tmp_file(date, station_id):
-    # create tmp file
-    filename = 's%d_' % station_id + date.strftime('%Y_%-m_%-d.h5')
-    filepath = os.path.join(settings.TMP_PATH, filename)
-    file = tables.openFile(filepath, 'w')
-
-    return file
