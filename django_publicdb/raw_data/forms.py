115344d69d3c0fdec87b31d3a3d6e8a9e8c765ab
Arne de Laat <adelaat@nikhef.nl>
2013-06-05 13:39:40 +0200

    Improve info and example for download
    Change example to dates for which there is actually data available
    Add warning to use dates after 14-5-2013
    ESD not yet available (for all stations) for earlier dates

--- django_publicdb/raw_data/forms.py
+++ django_publicdb/raw_data/forms.py
@@ -4,8 +4,8 @@ from django_publicdb.inforecords.models import Station
 
 class DataDownloadForm(forms.Form):
     station = forms.ModelChoiceField(Station.objects.all())
-    start = forms.DateTimeField(help_text="e.g. '2012-10-17', or "
-                                          "'2012-10-17 12:45'")
-    end = forms.DateTimeField(help_text="e.g. '2012-10-17', or "
-                                        "'2012-10-17 12:45'")
+    start = forms.DateTimeField(help_text="e.g. '2013-5-17', or "
+                                          "'2013-5-17 12:45'")
+    end = forms.DateTimeField(help_text="e.g. '2013-5-18', or "
+                                        "'2013-5-18 9:05'")
     download = forms.BooleanField(required=False)
