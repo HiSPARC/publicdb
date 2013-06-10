8bfa9069944ad3c089890f60a5d9abf8a9a022f8
Ching Bon Lam <cblam@nikhef.nl>
2013-06-10 21:16:32 +0200

    Moved code to branch 'api_quality_nagios'. Code for Nagios monitoring of pulseheight mpv moved to its own feature branch. This allows simple merging between master and this branch, instead of checking out specific files from this branch to master.

--- django_publicdb/inforecords/views.py
+++ django_publicdb/inforecords/views.py
@@ -10,8 +10,6 @@ from django_publicdb.inforecords.models import *
 from django_publicdb.histograms.models import *
 from django_publicdb.status_display.views import station_has_data
 
-import os
-
 @login_required
 def keys(request, host):
     """Return a zip-file containing the hosts OpenVPN keys"""
@@ -44,19 +42,11 @@ def create_nagios_config(request):
     hosts = []
     for host in (Pc.objects.exclude(type__description='Admin PC')
                            .filter(is_active=True)):
-
         services = []
         for service in host.enabledservice_set.all():
             check_command = service.monitor_service.nagios_command
-
-            # Skip pulseheight monitoring here, we will do it somewhere below
-            # because it requires its own custom check interval
-            if check_command.count('check_pulseheight'):
-                continue
-
             # Let's see if we need to pass parameters to this service
             if check_command.count('check_nrpe'):
-
                 # The following code will check four variables to see if
                 # they exist in the EnabledService instance 'service'.  If
                 # they do, assign the value locally using an exec
@@ -68,48 +58,24 @@ def create_nagios_config(request):
                         exec('%s = service.%s' % (var, var))
                     else:
                         exec('%s = service.monitor_service.%s' % (var, var))
-
                 # Append the parameters to the check command
                 check_command += ('!%s:%s!%s:%s' %
                                  (min_warning, max_warning, min_critical,
                                   max_critical))
-
             # Append this service to the hosts service list
             services.append(
                 {'description': service.monitor_service.description,
                  'check_command': check_command,
                  'active_checks': service.monitor_service.enable_active_checks})
-
         has_data = station_has_data(host.station)
-
-        # Add the pulseheight monitoring service here
-        pulseheight_thresholds = []
-        for service in host.enabledservice_set.all():
-            check_command = service.monitor_service.nagios_command
-
-            if not check_command.count('check_pulseheight_mpv'):
-                continue
-
-            pulseheight_thresholds = MonitorPulseheightThresholds.objects.filter(station=host.station)
-
         # Append this host to the hosts list
-        hosts.append({
-            'pc'                    : host,
-            'services'              : services,
-            'pulseheight_thresholds': pulseheight_thresholds,
-            'has_data'              : has_data
-        })
-
-    # Add the publicdb/scripts path
-    scripts_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                                "..", "..", "scripts"))
+        hosts.append({'pc': host, 'services': services, 'has_data': has_data})
 
     # Render the template
     return render_to_response('nagios.cfg',
                               {'contacts': Contact.objects.all(),
                                'clusters': Cluster.objects.all(),
-                               'hosts': hosts,
-                               'scripts_path': scripts_path},
+                               'hosts': hosts},
                               mimetype='text/plain')
 
 
