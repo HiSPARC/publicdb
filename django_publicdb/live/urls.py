67e62bdd88b8711af1aa64fd7533ffc895a13c7b
Arne de Laat <adelaat@nikhef.nl>
2013-06-21 17:25:17 +0200

    Update live display for Django 1.5

--- django_publicdb/live/urls.py
+++ django_publicdb/live/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns
 
 urlpatterns = patterns('django_publicdb.live.views',
     (r'^stations/(?P<station_id>\d+)/$', 'station'),
