46babe1d7290c5ac0d89dd6b27b3661677c9f527
Arne de Laat <adelaat@nikhef.nl>
2013-07-02 15:46:01 +0200

    Add FIXME for using the wrong code
    Currently a script (bazaar?) clones the publicdb repo into the box
    But we want to test latest dev version, so the local repo.
    Which is shared with the box, as described in the Vagrantfile

--- vagrant/puppet/manifests/bazaar.sh
+++ vagrant/puppet/manifests/bazaar.sh
@@ -9,6 +9,8 @@ sudo chmod g+rwx publicdb
 sudo setfacl -m d:g::rwx publicdb
 
 cd /srv/publicdb
+# FIXME: We should use the shared code from the host machine,
+#  not the GitHub repo, since it does not have the local changes. 
 git clone https://github.com/HiSPARC/publicdb.git www
 sudo chmod g+w www
 sudo mkdir static
