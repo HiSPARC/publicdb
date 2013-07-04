f63ca6762827a70b05904223decea456b6c6b240
Arne de Laat <adelaat@nikhef.nl>
2013-07-04 08:47:41 +0200

    Add option to the Data Download Form for weather data
    Add new /weather url to download weather data

--- django_publicdb/raw_data/forms.py
+++ django_publicdb/raw_data/forms.py
@@ -2,10 +2,16 @@ from django import forms
 
 from django_publicdb.inforecords.models import Station
 
+
+TYPES = [('events','Events'),
+         ('weather','Weather')]
+
+
 class DataDownloadForm(forms.Form):
     station = forms.ModelChoiceField(Station.objects.all())
     start = forms.DateTimeField(help_text="e.g. '2013-5-17', or "
                                           "'2013-5-17 12:45'")
     end = forms.DateTimeField(help_text="e.g. '2013-5-18', or "
                                         "'2013-5-18 9:05'")
+    data_type = forms.ChoiceField(choices=TYPES, widget=forms.RadioSelect())
     download = forms.BooleanField(required=False)
--- django_publicdb/raw_data/urls.py
+++ django_publicdb/raw_data/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('django_publicdb.raw_data',
     (r'^download$', 'views.download_form'),
     (r'^download/(?P<station_id>\d+)/(?P<start>[^/]+)/(?P<end>[^/]+)$', 'views.download_form'),
     (r'^rpc$', 'views.call_xmlrpc'),
-    (r'^(?P<station_id>\d+)/events$', 'views.download_events'),
+    (r'^(?P<station_id>\d+)/events$', 'views.download_data', {'data_type': 'events'}),
+    (r'^(?P<station_id>\d+)/weather$', 'views.download_data', {'data_type': 'weather'}),
 )
