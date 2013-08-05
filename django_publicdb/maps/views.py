ffeee3f51f14bb739dda5408537ad4aae09b01f2
Arne de Laat <adelaat@nikhef.nl>
2013-08-05 20:57:46 +0200

    Also reduce queries for maps
    Get related objects in one query

--- django_publicdb/maps/views.py
+++ django_publicdb/maps/views.py
@@ -22,9 +22,11 @@ def station_on_map(request, station_id):
     subclusters = []
     for subcluster in Cluster.objects.all():
         stations = []
-        for station in Station.objects.filter(cluster=subcluster,
-                                              pc__is_active=True,
-                                              pc__is_test=False):
+        for station in (Station.objects.select_related('cluster__parent',
+                                                       'cluster__country')
+                                       .filter(cluster=subcluster,
+                                               pc__is_active=True,
+                                               pc__is_test=False)):
             detector = (DetectorHisparc.objects.filter(station=station,
                                                        startdate__lte=today)
                                                .latest('startdate'))
@@ -72,9 +74,11 @@ def stations_on_map(request, country=None, cluster=None, subcluster=None):
     subclusters = []
     for subcluster in Cluster.objects.all():
         stations = []
-        for station in Station.objects.filter(cluster=subcluster,
-                                              pc__is_active=True,
-                                              pc__is_test=False):
+        for station in (Station.objects.select_related('cluster__parent',
+                                                       'cluster__country')
+                                       .filter(cluster=subcluster,
+                                               pc__is_active=True,
+                                               pc__is_test=False)):
             detector = (DetectorHisparc.objects.filter(station=station,
                                                        startdate__lte=today)
                                                .latest('startdate'))
