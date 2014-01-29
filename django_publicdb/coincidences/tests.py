971d2703c99a5b42a47ac4aaa33c00f2e4bf6cc2
Arne de Laat <adelaat@nikhef.nl>
2014-01-29 12:33:12 +0100

    Use assertEqual instead of assert in coincidences tests
    Also PEP8 fixes

--- django_publicdb/coincidences/tests.py
+++ django_publicdb/coincidences/tests.py
@@ -7,12 +7,7 @@ from django_publicdb.inforecords.models import *
 
 class SerializationTestCase(TestCase):
 
-    fixtures = [
-        'tests_inforecords']
-
-    #--------------------------------------------------------------------------
-    # Setup and teardown
-    #--------------------------------------------------------------------------
+    fixtures = ['tests_inforecords']
 
     def setUp(self):
         super(SerializationTestCase, self).setUp()
@@ -20,70 +15,62 @@ class SerializationTestCase(TestCase):
     def tearDown(self):
         super(SerializationTestCase, self).tearDown()
 
-    #--------------------------------------------------------------------------
-    # Tests
-    #--------------------------------------------------------------------------
-
-    def test_Event_Serialization_JSON(self):
+    def test_event_serialization_json(self):
 
         # Create an event and save it to the database
 
         station = Station.objects.all()[0]
 
-        event = Event(date = "2010-05-01",
-                      time = "00:09:15",
-                      nanoseconds = 665162853,
-                      station = station,
-                      pulseheights = [1, 2, 3, 4],
-                      integrals = [1, 2, 3, 4],
-                      traces = [[1, 2, 3, 4]])
+        event = Event(date="2010-05-01",
+                      time="00:09:15",
+                      nanoseconds=665162853,
+                      station=station,
+                      pulseheights=[1, 2, 3, 4],
+                      integrals=[1, 2, 3, 4],
+                      traces=[[1, 2, 3, 4], [1, 2, 3, 4]])
 
         event.save()
 
-        assert len(Event.objects.all()) == 1
+        self.assertEqual(len(Event.objects.all()), 1)
 
         # Retrieve the first event and serialize it
 
         original_event = Event.objects.all()[0]
-
         serialized_event = serializers.serialize("json", [original_event])
 
         # Deserialize the event
 
         iterator = serializers.deserialize("json", serialized_event)
-
         deserialized_event = iterator.next().object
 
         self.assertEqual(original_event, deserialized_event)
 
-    def test_Event_Serialization_XML(self):
+    def test_event_serialization_xml(self):
 
         # Create an event and save it to the database
 
         station = Station.objects.all()[0]
 
-        event = Event(date = "2010-05-01",
-                      time = "00:09:15",
-                      nanoseconds = 665162853,
-                      station = station,
-                      pulseheights = [1, 2, 3, 4],
-                      integrals = [1, 2, 3, 4],
-                      traces = [[1, 2, 3, 4]])
+        event = Event(date="2010-05-01",
+                      time="00:09:15",
+                      nanoseconds=665162853,
+                      station=station,
+                      pulseheights=[1, 2, 3, 4],
+                      integrals=[1, 2, 3, 4],
+                      traces=[[1, 2, 3, 4], [1, 2, 3, 4]])
 
         event.save()
 
-        assert len(Event.objects.all()) == 1
+        self.assertEqual(len(Event.objects.all()), 1)
 
         # Retrieve the first event and serialize it
 
         original_event = Event.objects.all()[0]
-
         serialized_event = serializers.serialize("xml", [original_event])
 
         # Deserialize the event
 
         iterator = serializers.deserialize("xml", serialized_event)
-
         deserialized_event = iterator.next().object
 
         self.assertEqual(original_event, deserialized_event)
