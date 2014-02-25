e5abb122bce09a9dc6181cba55f35ff37be09cb9
David Fokkema <davidf@nikhef.nl>
2014-02-25 22:29:54 +0100

    Remove absolute URLs in settings.py and restart uWSGI

--- provisioning/settings.py
+++ provisioning/settings.py
@@ -82,7 +82,7 @@ MEDIA_ROOT = '/localstore/media/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = 'http://192.168.10.10/media/'
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -92,7 +92,7 @@ STATIC_ROOT = '/srv/publicdb/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = 'http://192.168.10.10/media/static/'
+STATIC_URL = '/media/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
