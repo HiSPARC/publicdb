b1577ae4f4d62c1d68d89479bba4e787032aaa45
David Fokkema <davidf@nikhef.nl>
2014-02-28 11:06:43 +0100

    Add hisparc user to SLC6.4 box

--- veewee/definitions/SLC6.4/vagrant.sh
+++ veewee/definitions/SLC6.4/vagrant.sh
@@ -16,3 +16,20 @@ chown -R vagrant /home/vagrant/.ssh
 
 # Customize the message of the day
 echo 'Welcome to your Vagrant-built virtual machine.' > /etc/motd
+
+
+#
+# The real server will have a hisparc user
+#
+
+# Add user hisparc with sudo access
+/usr/sbin/adduser hisparc
+echo "hisparc"|passwd --stdin hisparc
+echo "hisparc        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
+
+# Installing vagrant keys for hisparc user
+mkdir /home/hisparc/.ssh
+chmod 700 /home/hisparc/.ssh
+cd /home/hisparc/.ssh
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
+chown -R hisparc /home/hisparc/.ssh
