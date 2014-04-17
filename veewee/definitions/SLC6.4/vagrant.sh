80c3e9dc70a9c02bcd452ad2977cd5cc0141fbbd
David Fokkema <davidf@nikhef.nl>
2014-04-17 09:13:35 +0200

    Pruned veewee/vagrant customizations

--- veewee/definitions/SLC6.4/vagrant.sh
+++ veewee/definitions/SLC6.4/vagrant.sh
@@ -1,19 +1,6 @@
 # Vagrant specific
 date > /etc/vagrant_box_build_time
 
-# Add vagrant user
-/usr/sbin/groupadd vagrant
-/usr/sbin/useradd vagrant -g vagrant -G wheel
-echo "vagrant"|passwd --stdin vagrant
-echo "vagrant        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers.d/vagrant
-chmod 0440 /etc/sudoers.d/vagrant
-
-# Installing vagrant keys
-mkdir -pm 700 /home/vagrant/.ssh
-wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O /home/vagrant/.ssh/authorized_keys
-chmod 0600 /home/vagrant/.ssh/authorized_keys
-chown -R vagrant /home/vagrant/.ssh
-
 # Customize the message of the day
 echo 'Welcome to your Vagrant-built virtual machine.' > /etc/motd
 
@@ -21,11 +8,6 @@ echo 'Welcome to your Vagrant-built virtual machine.' > /etc/motd
 #
 # The real server will have a hisparc user
 #
-
-# Add user hisparc with sudo access
-/usr/sbin/adduser hisparc
-echo "hisparc"|passwd --stdin hisparc
-echo "hisparc        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
 
 # Installing vagrant keys for hisparc user
 mkdir /home/hisparc/.ssh
