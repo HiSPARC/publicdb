66b9fdc425bdf7011b47f6a864aff5d5e7f39d91
Arne de Laat <adelaat@nikhef.nl>
2014-05-01 00:52:54 +0200

    Add downloading of coincidences as csv
    Add a download form and streaming csv to download coincidences.
    Each row in the csv contains an event, station number and coincidence
    id.
    Using the coincidence ids the events belonging to the same
    coincidence can be found.

--- django_publicdb/raw_data/forms.py
+++ django_publicdb/raw_data/forms.py
@@ -15,3 +15,12 @@ class DataDownloadForm(forms.Form):
                                         "'2013-5-18 9:05'")
     data_type = forms.ChoiceField(choices=TYPES, widget=forms.RadioSelect())
     download = forms.BooleanField(required=False)
+
+
+class CoincidenceDownloadForm(forms.Form):
+    start = forms.DateTimeField(help_text="e.g. '2014-4-5', or "
+                                          "'2014-4-18 12:45'")
+    end = forms.DateTimeField(help_text="e.g. '2014-4-29', or "
+                                        "'2014-04-30 9:05'")
+    n = forms.IntegerField(min_value=2)
+    download = forms.BooleanField(required=False)
