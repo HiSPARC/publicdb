078d7f93ab6d2b50db5d759e7a7896f1638d100f
Arne de Laat <adelaat@nikhef.nl>
2014-05-28 01:01:39 +0200

    Make the date test use UTC time
    That way it will more accurately check if a day has passed.
    HiSPARC events should be based on GPS time,
    which is around 16 seconds offset from UTC time.

--- django_publicdb/histograms/datastore.py
+++ django_publicdb/histograms/datastore.py
@@ -45,7 +45,7 @@ def get_updated_files(rootdir, last_check_time):
                                         file, '%Y_%m_%d.h5').date()
                         except ValueError:
                             continue
-                        if date != datetime.date.today():
+                        if date != datetime.datetime.utcnow().date():
                             file_list.append((date, file_path))
 
     return file_list
