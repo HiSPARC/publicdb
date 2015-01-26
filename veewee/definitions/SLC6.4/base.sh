0f80332abaf64f6c3335b308a1b228206aa1badd
David Fokkema <davidf@nikhef.nl>
2015-01-26 16:41:35 +0100

    Retire veewee

--- veewee/definitions/SLC6.4/base.sh
+++
@@ -1,5 +0,0 @@
-# Base install
-
-sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
-
-yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget dkms
