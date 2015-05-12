dcc85ac755a4c5b1daab5c5886ace80476d7a611
Arne de Laat <adelaat@nikhef.nl>
2015-05-12 12:25:58 +0200

    Rename detector offsets csv and fix its view

--- django_publicdb/status_display/views.py
+++ django_publicdb/status_display/views.py
@@ -625,6 +625,26 @@ def get_gps_config_source(request, station_number):
     return response
 
 
+def get_detector_timing_offsets_source(station_number):
+    offsets = (DetectorTimingOffset.objects.filter(
+        source__station__number=station_number,
+        timestamp__gte=FIRSTDATE,
+        timestamp__lte=datetime.date.today()).order_by('timestamp'))
+
+    data = offsets.values_list('source__date', 'offset_1', 'offset_2',
+                               'offset_3', 'offset_4')
+    data = [[calendar.timegm(row[0].utctimetuple()), row[1], row[2],
+             row[3], row[4]] for row in data]
+
+    response = render_to_response('source_detector_timing_offsets.csv',
+                                  {'data': data,
+                                   'station_number': station_number},
+                                  mimetype='text/csv')
+    response['Content-Disposition'] = (
+        'attachment; filename=detector_timing_offsets-s%s.csv' % station_number)
+    return response
+
+
 def get_histogram_source(year, month, day, type, station_number=None):
     date = datetime.date(int(year), int(month), int(day))
     if station_number is None:
@@ -676,20 +696,6 @@ def get_config_source(station_number, type):
                  row[3]] for row in data]
     else:
         data = None
-
-    return data
-
-
-def get_detector_timing_offsets_source(station_number):
-    offsets = (DetectorTimingOffset.objects.filter(
-        source__station__number=station_number,
-        timestamp__gte=FIRSTDATE,
-        timestamp__lte=datetime.date.today()).order_by('timestamp'))
-
-    data = offsets.values_list('source__date', 'offset_1', 'offset_2',
-                               'offset_3', 'offset_4')
-    data = [[calendar.timegm(row[0].utctimetuple()), row[1], row[2],
-             row[3], row[4]] for row in data]
 
     return data
 
