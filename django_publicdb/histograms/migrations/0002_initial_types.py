c8d1e3e05a0fb0749321f64f147614e31ba88486
Arne de Laat <adelaat@nikhef.nl>
2015-05-15 13:26:28 +0200

    Update loading initial data
    Histogram and dataset types are now loaded by a migration.
    
    The reverse code is intentionally not used:
    Reversing this migration with unload_fixture would remove all types
    from the database, which would also remove all associated Daily
    histograms/datasets!

---
+++ django_publicdb/histograms/migrations/0002_initial_types.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.core.management import call_command
+
+
+def load_fixture(apps, schema_editor):
+    """Load initial data for histogram and dataset types from the fixture"""
+
+    call_command('loaddata', 'initial_types.json', app_label='histograms')
+
+
+def unload_fixture(apps, schema_editor):
+    """Delete all entries from the histogram/dataset types
+
+    This will also delete all related histograms/datasets!
+
+    """
+    HistogramType = apps.get_model('histograms', 'HistogramType')
+    HistogramType.objects.all().delete()
+    DatasetType = apps.get_model('histograms', 'DatasetType')
+    DatasetType.objects.all().delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('histograms', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(load_fixture) # reverse_code=unload_fixture
+    ]
