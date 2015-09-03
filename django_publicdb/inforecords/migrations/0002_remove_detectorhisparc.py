17d710fa83b2e2224ebb3ce8b9d8de7bf19f36ed
Arne de Laat <adelaat@nikhef.nl>
2015-09-03 17:52:00 +0200

    Add migrations to remove DetectorHisparc and Meta changes

---
+++ django_publicdb/inforecords/migrations/0002_remove_detectorhisparc.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('inforecords', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterUniqueTogether(
+            name='detectorhisparc',
+            unique_together=None,
+        ),
+        migrations.RemoveField(
+            model_name='detectorhisparc',
+            name='station',
+        ),
+        migrations.DeleteModel(
+            name='DetectorHisparc',
+        ),
+    ]
