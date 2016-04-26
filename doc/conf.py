5dc296f243db3a351384e7545e2dc79f3310dd64
Arne de Laat <arne@delaat.net>
2016-04-26 21:44:37 +0200

    Add calibration module to MOCK modules for docs

--- doc/conf.py
+++ doc/conf.py
@@ -13,9 +13,10 @@ import os
 import mock
 
 MOCK_MODULES = ['numpy', 'scipy', 'tables',
-                'sapphire', 'sapphire.utils', 'sapphire.analysis',
-                'sapphire.analysis.event_utils', 'sapphire.storage',
-                'sapphire.transformations']
+                'sapphire', 'sapphire.utils', 'sapphire.storage',
+                'sapphire.transformations', 'sapphire.analysis',
+                'sapphire.analysis.event_utils',
+                'sapphire.analysis.calibration']
 sys.modules.update((name, mock.MagicMock()) for name in MOCK_MODULES)
 
 # The directory that contains settings_develop.py
