9744c1a0839af3e2cce190313971804941ed3270
Arne de Laat <adelaat@nikhef.nl>
2016-05-17 14:43:06 +0200

    Add migration for StationTimingOffset unique_together

---
+++ django_publicdb/histograms/migrations/0007_unique_stationtimingoffset.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('histograms', '0006_stationtimingoffset'),
+    ]
+
+    operations = [
+        migrations.AlterUniqueTogether(
+            name='stationtimingoffset',
+            unique_together=set([('ref_source', 'source')]),
+        ),
+    ]
+
