e22ced62b8a9b0d3d318cbe49eb0a90485ad87ea
Arne de Laat <adelaat@nikhef.nl>
2016-10-11 23:33:35 +0200

    Replace OpenLayers maps by Leaflet

---
+++ django_publicdb/default/static/scripts/leaflet.hisparc.js
@@ -0,0 +1,35 @@
+
+function createMap(name) {
+    var map = L.map(name, {
+        center: L.latLng(52.3559, 4.951),
+        zoom: 5,
+        zoomSnap: 0.2,
+        zoomDelta: 0.4
+    });
+
+    L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+        attribution: '<a href="http://data.hisparc.nl/" title="HiSPARC data">HiSPARC</a><br>' +
+                     '<a href="http://openstreetmap.org/copyright" title="© OpenStreetMap contributors">OSM</a> | ' +
+                     '<a href="https://carto.com/attributions" title="© CARTO Positron">CARTO</a>'
+    }).addTo(map);
+    L.control.scale({imperial: false}).addTo(map);
+
+    return map;
+}
+
+var statusColors = {
+    down: 'red',
+    problem: 'gold',
+    up: 'green',
+    unknown: 'grey',
+    gpsold: '#EE9911',
+    gpsnew: '#2A7AE2'
+};
+
+var pointStyle = {
+    radius: 7,
+    weight: 1.5,
+    fillOpacity: 0.6
+};
+
+var boundsPadding = [15, 15];
