4c273b6dd299a7e11ff63bcbd36d118bffe3be18
Tom Kooij <tomkooij@tomkooij.nl>
2017-02-27 09:57:28 +0100

    Revert "Delete datastore-config-server.py"
    This reverts commit a504f84073052d6be4c8ea65f24faab8f1ba1061.
    
    The script datastore-config-server.py has jinja {} templates in it.
    It needs to be here.
    There is a version in datastore/examples now for reference.

---
+++ provisioning/datastore-config-server.py
@@ -0,0 +1,65 @@
+#!/usr/local/bin/python
+""" Simple XML-RPC Server to run on the datastore server.
+
+    This daemon should be run on HiSPARC's datastore server.  It will
+    handle the cluster layouts and station passwords.  When an update is
+    necessary, it will reload the HTTP daemon.
+
+    The basis for this code was ripped from the python SimpleXMLRPCServer
+    library documentation and extended.
+
+"""
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
+import urllib2
+import hashlib
+import subprocess
+import os
+
+HASH = '/tmp/hash_datastore'
+DATASTORE_CFG = '/databases/frome/station_list.csv'
+CFG_URL = '{{ datastore_config_url }}'
+RELOAD_PATH = '/tmp/uwsgi-reload.me'
+
+def reload_datastore():
+    """Load datastore config and reload datastore, if necessary"""
+
+    datastore_cfg = urllib2.urlopen(CFG_URL).read()
+    new_hash = hashlib.sha1(datastore_cfg).hexdigest()
+
+    try:
+        with open(HASH, 'r') as file:
+            old_hash = file.readline()
+    except IOError:
+        old_hash = None
+
+    if new_hash == old_hash:
+        return True
+    else:
+        with open(DATASTORE_CFG, 'w') as file:
+            file.write(datastore_cfg)
+
+        # reload uWSGI
+        with open(RELOAD_PATH, 'a'):
+            os.utime(RELOAD_PATH, None)
+
+        with open(HASH, 'w') as file:
+            file.write(new_hash)
+
+    return True
+
+
+if __name__ == '__main__':
+    # Restrict to a particular path.
+    class RequestHandler(SimpleXMLRPCRequestHandler):
+        rpc_paths = ('/RPC2',)
+
+    # Create server
+    server = SimpleXMLRPCServer(('{{ datastore_host }}', {{ datastore_port }}),
+                                requestHandler=RequestHandler)
+    server.register_introspection_functions()
+
+    server.register_function(reload_datastore)
+
+    # Run the server's main loop
+    server.serve_forever()
