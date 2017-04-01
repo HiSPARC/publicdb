3f8f86afc3aa50a7fb38edd3e3498c295facd43e
Arne de Laat <arne@delaat.net>
2017-04-01 20:11:07 +0200

    Make trailing slash optional for update urls to reduce redirects.

--- publicdb/updates/urls.py
+++ publicdb/updates/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^installer/latest/$', views.get_latest_installer),
     url(r'^(?P<queue>\w+)/check$', views.update_check_querystring),
-    url(r'^(?P<queue>\w+)/check/(?P<admin_version>\d+)/(?P<user_version>\d+)/$', views.update_check),
+    url(r'^(?P<queue>\w+)/check/(?P<admin_version>\d+)/(?P<user_version>\d+)/?$', views.update_check),
 ]
