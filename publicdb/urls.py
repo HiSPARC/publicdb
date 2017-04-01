ad6ddc6d9e84976570f39cc74dcaa72abb24a418
Arne de Laat <arne@delaat.net>
2017-04-01 10:41:27 +0200

    Fix missing url() in main urls.py

--- publicdb/urls.py
+++ publicdb/urls.py
@@ -5,24 +5,24 @@ from django.views.generic import RedirectView, TemplateView
 from .inforecords.views import create_nagios_config, create_datastore_config, keys
 
 urlpatterns = [
-    (r'^$', RedirectView.as_view(url='show/stations', permanent=False)),
+    url(r'^$', RedirectView.as_view(url='show/stations', permanent=False)),
 
-    (r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 
-    (r'^api/', include('publicdb.api.urls')),
-    (r'^show/', include('publicdb.status_display.urls')),
-    (r'^maps/', include('publicdb.maps.urls')),
-    (r'^layout/', include('publicdb.station_layout.urls')),
-    (r'^analysis-session/', include('publicdb.analysissessions.urls')),
-    (r'^jsparc/', include('publicdb.jsparc.urls')),
-    (r'^software-updates/', include('publicdb.updates.urls')),
-    (r'^raw_data/', include('publicdb.raw_data.urls')),
-    (r'^data/', include('publicdb.raw_data.urls')),
+    url(r'^api/', include('publicdb.api.urls')),
+    url(r'^show/', include('publicdb.status_display.urls')),
+    url(r'^maps/', include('publicdb.maps.urls')),
+    url(r'^layout/', include('publicdb.station_layout.urls')),
+    url(r'^analysis-session/', include('publicdb.analysissessions.urls')),
+    url(r'^jsparc/', include('publicdb.jsparc.urls')),
+    url(r'^software-updates/', include('publicdb.updates.urls')),
+    url(r'^raw_data/', include('publicdb.raw_data.urls')),
+    url(r'^data/', include('publicdb.raw_data.urls')),
 
-    (r'^config/nagios$', create_nagios_config),
-    (r'^config/datastore$', create_datastore_config),
+    url(r'^config/nagios$', create_nagios_config),
+    url(r'^config/datastore$', create_datastore_config),
 
-    (r'^keys/(?P<host>\w+)/$', keys),
+    url(r'^keys/(?P<host>\w+)/$', keys),
 
-    (r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 ]
