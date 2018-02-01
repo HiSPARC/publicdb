e1670540104dbb87db873517ee4e8996cab03a83
Arne de Laat <arne@delaat.net>
2018-02-01 08:44:06 +0100

    Add tests for maps.

---
+++ tests/test_maps/test_views.py
@@ -0,0 +1,36 @@
+from django.test import Client, TestCase
+from django.urls import reverse
+
+from ..factories import histograms_factories
+from ..factories.inforecords_factories import StationFactory
+
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.station = StationFactory(number=1, cluster__number=0, cluster__country__number=0)
+        self.summary = histograms_factories.SummaryFactory(station=self.station)
+        self.config = histograms_factories.ConfigurationFactory(source=self.summary)
+        super(TestViews, self).setUp()
+
+    def get_html(self, url):
+        """Get url and check if the response is OK and valid json"""
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('text/html', response['Content-Type'])
+        return response
+
+    def test_station_on_map(self):
+        kwargs = {'station_number': self.station.number}
+        self.get_html(reverse('maps:map', kwargs=kwargs))
+
+    def test_all_stations_on_map(self):
+        self.get_html(reverse('maps:map'))
+
+    def test_stations_on_map(self):
+        kwargs = {'country': self.station.cluster.country.name}
+        self.get_html(reverse('maps:map', kwargs=kwargs))
+        kwargs.update({'cluster': self.station.cluster.main_cluster()})
+        self.get_html(reverse('maps:map', kwargs=kwargs))
+        kwargs.update({'subcluster': self.station.cluster.name})
+        self.get_html(reverse('maps:map', kwargs=kwargs))
