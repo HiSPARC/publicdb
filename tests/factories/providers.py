2be5474dd0722b603c6c2d581136d253c2dc9a75
Arne de Laat <arne@delaat.net>
2018-02-16 17:00:12 +0100

    Round generated floats to prevent equality issues in tests.

--- tests/factories/providers.py
+++ tests/factories/providers.py
@@ -12,8 +12,8 @@ def make_urlsafe(value):
 class DataProvider(BaseProvider):
     """Provider for histogram and dataset data"""
 
-    def float(self, min=0., max=100.):
-        return self.generator.random.uniform(a=min, b=max)
+    def float(self, min=0., max=100., ndigits=6):
+        return round(self.generator.random.uniform(a=min, b=max), ndigits)
 
     def int_list(self, n=24, **kwargs):
         return [self.random_int(**kwargs) for _ in range(n)]
