30dd135e9a9b5378aa71ac306c280aab7eb73c1b
David Fokkema <davidf@nikhef.nl>
2018-07-03 15:03:54 +0200

    Change status from closure to class
    We need more methods.

--- publicdb/status_display/status.py
+++ publicdb/status_display/status.py
@@ -7,28 +7,37 @@ RECENT_NUM_DAYS = 7
 RECENT_THRESHOLD = 4
 
 
-def get_status_func():
-    """Return function to query the status of a station.
+class StationStatus(object):
 
-    The status can be queried by calling function(station_number). If the
-    station has data as recent as yesterday, it is considered 'up'. If it has
-    at least RECENT_THRESHOLD days of data in the last RECENT_NUM_DAYS, it is
-    considered 'problem'. If it has less data than the threshold, it is
-    considered 'down'.
+    """Query the status of a station.
+
+    You can query the status of a single station, or get the status counts for
+    all stations.
 
     """
-    yesterday = datetime.date.today() - datetime.timedelta(days=1)
-    recent_day = datetime.date.today() - datetime.timedelta(days=RECENT_NUM_DAYS)
 
-    stations_with_current_data = [x.station.number for x in Summary.objects.filter(date__exact=yesterday)]
-    stations_with_recent_data = [x.station.number for x in Summary.objects.filter(date__gte=recent_day)]
+    def __init__(self):
+        yesterday = datetime.date.today() - datetime.timedelta(days=1)
+        recent_day = datetime.date.today() - datetime.timedelta(days=RECENT_NUM_DAYS)
 
-    def get_status(station_number):
-        if station_number in stations_with_current_data:
+        self.stations_with_current_data = [x.station.number for x in Summary.objects.filter(date__exact=yesterday)]
+        self.stations_with_recent_data = [x.station.number for x in Summary.objects.filter(date__gte=recent_day)]
+
+    def get_status(self, station_number):
+        """Query station status.
+
+        If the station has data as recent as yesterday, it is considered 'up'.
+        If it has at least RECENT_THRESHOLD days of data in the last
+        RECENT_NUM_DAYS, it is considered 'problem'. If it has less data than
+        the threshold, it is considered 'down'.
+
+        :param station_number: the station for which to get the statuscount
+        :return: 'up', 'problem', or 'down'
+
+        """
+        if station_number in self.stations_with_current_data:
             return 'up'
-        elif stations_with_recent_data.count(station_number) > RECENT_THRESHOLD:
+        elif self.stations_with_recent_data.count(station_number) > RECENT_THRESHOLD:
             return 'problem'
         else:
             return 'down'
-
-    return get_status
