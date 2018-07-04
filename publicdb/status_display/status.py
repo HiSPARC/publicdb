7114261d0576a835d660f8133acf4231b8b0cdc4
David Fokkema <davidf@nikhef.nl>
2018-07-04 13:53:28 +0200

    Use 'unknown' for stations without an active PC

--- publicdb/status_display/status.py
+++ publicdb/status_display/status.py
@@ -1,6 +1,6 @@
 import datetime
 
-from publicdb.inforecords.models import Station
+from publicdb.inforecords.models import Station, Pc
 from publicdb.histograms.models import Summary
 
 
@@ -24,6 +24,7 @@ class StationStatus(object):
         self.stations = Station.objects.values_list('number', flat=True)
         self.stations_with_current_data = Summary.objects.filter(date__exact=yesterday).values_list('station__number', flat=True)
         self.stations_with_recent_data = list(Summary.objects.filter(date__gte=recent_day).values_list('station__number', flat=True))
+        self.stations_with_pc = Pc.objects.exclude(type__slug='admin').filter(is_active=True).values_list('station__number', flat=True)
 
     def get_status(self, station_number):
         """Query station status.
@@ -41,6 +42,8 @@ class StationStatus(object):
             return 'up'
         elif self.stations_with_recent_data.count(station_number) >= RECENT_THRESHOLD:
             return 'problem'
+        elif station_number not in self.stations_with_pc:
+            return 'unknown'
         else:
             return 'down'
 
