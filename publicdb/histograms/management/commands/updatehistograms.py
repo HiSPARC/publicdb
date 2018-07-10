f584952b08f57d392cb6e110fd80f8e542839389
Arne de Laat <arne@delaat.net>
2018-07-10 23:16:32 +0200

    Add test for updatehistograms. Make loggers for unfinished previous runs warnings.

--- publicdb/histograms/management/commands/updatehistograms.py
+++ publicdb/histograms/management/commands/updatehistograms.py
@@ -12,16 +12,17 @@ class Command(BaseCommand):
 
     def handle(*args, **options):
         logger.info("Checking for new events...")
-        status = check_for_updates()
-        if status is False:
-            logger.info("Check has not completed a previous run yet")
+        has_run = check_for_updates()
+        if not has_run:
+            logger.warning("Check has not completed a previous run yet")
         else:
             logger.info("Update check finished.")
 
         logger.info("Building new histograms...")
         completed = update_all_histograms()
         if not completed:
-            logger.info("Histograms did not complete a previous run yet")
+            logger.warning("Histograms did not complete a previous run yet")
         else:
             logger.info("Finished building histograms")
+
         logger.info("Done.")
