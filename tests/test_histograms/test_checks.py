9a9016b9eaf1c72256d409af780f1e43100cff19
Arne de Laat <arne@delaat.net>
2018-07-26 22:43:38 +0200

    Add test for checks updating existing summary.

--- tests/test_histograms/test_checks.py
+++ tests/test_histograms/test_checks.py
@@ -7,6 +7,7 @@ from publicdb.histograms import checks, models
 
 from ..factories import histograms_factories, inforecords_factories
 
+
 class TestChecks(TestCase):
 
     @patch('publicdb.histograms.checks.check_for_new_events_and_update_flags')
@@ -56,6 +57,50 @@ class TestChecks(TestCase):
         self.assertGreater(state.check_last_run, datetime(2004, 1, 1, 1, 0, 0))
 
         summary = models.Summary.objects.get(station=self.station, date=date(2017, 1, 1))
+        self.assertEqual(summary.num_events, 168)
+        self.assertTrue(summary.needs_update_events)
+        self.assertEqual(summary.num_weather, 60)
+        self.assertTrue(summary.needs_update_weather)
+        self.assertIsNone(summary.num_config)  # Not set 'early'
+        self.assertTrue(summary.needs_update_config)
+        self.assertEqual(summary.num_singles, 301)
+        self.assertTrue(summary.needs_update_singles)
+
+        network_summary = models.NetworkSummary.objects.get(date=date(2017, 1, 1))
+        self.assertTrue(network_summary.needs_update_coincidences)
+
+    @patch('publicdb.histograms.datastore.check_for_new_events')
+    def test_check_for_new_events_and_update_flags_updates_summaries(self, mock_events):
+        mock_events.return_value = {
+            date(2017, 1, 1): {
+                501: {
+                    'blobs': 687,
+                    'singles': 301,
+                    'weather': 60,
+                    'satellites': 0,
+                    'singles_old': 86400,
+                    'config': 1,
+                    'events': 168
+                }
+            }
+        }
+        self.setup_station()
+        summary = histograms_factories.SummaryFactory(
+            station=self.station, date=date(2017, 1, 1),
+            num_events=100, needs_update_events=False,
+            num_weather=None, needs_update_weather=False,
+            num_config=None, needs_update_config=False,
+            num_singles=10, needs_update_singles=False,
+        )
+
+        state = Mock(check_last_run=datetime(2004, 1, 1, 1, 0, 0))
+
+        checks.check_for_new_events_and_update_flags(state)
+        state.save.assert_called()
+        self.assertFalse(state.check_is_running)
+        self.assertGreater(state.check_last_run, datetime(2004, 1, 1, 1, 0, 0))
+
+        summary.refresh_from_db()
         self.assertEqual(summary.num_events, 168)
         self.assertTrue(summary.needs_update_events)
         self.assertEqual(summary.num_weather, 60)
