a2f59a0a5e6c5f7e1a46d9d1b01d7d1bc56f4a36
Arne de Laat <arne@delaat.net>
2018-09-07 15:23:04 +0200

    Improve form errors and add more tests for coincidence download form.

--- tests/test_raw_data/test_forms.py
+++ tests/test_raw_data/test_forms.py
@@ -49,8 +49,9 @@ class TestCoincidenceDownloadForm(TestCase):
 
     def setUp(self):
         # Required models
-        cluster = inforecords_factories.ClusterFactory(name='Amsterdam', number=0, country__number=0)
-        self.station = inforecords_factories.StationFactory(name='Nikhef', number=501, cluster=cluster)
+        self.cluster = inforecords_factories.ClusterFactory(name='Amsterdam', number=0, country__number=0)
+        self.station = inforecords_factories.StationFactory(name='Nikhef', number=501, cluster=self.cluster)
+        self.station2 = inforecords_factories.StationFactory(name='Nikhef2', number=502, cluster=self.cluster)
         self.summary = histograms_factories.SummaryFactory(
             station=self.station, date=datetime.date(2017, 1, 1),
             needs_update_events=False, num_events=168,
@@ -63,6 +64,9 @@ class TestCoincidenceDownloadForm(TestCase):
     def test_clean_valid(self):
         valid_form_data = [
             {'filter_by': 'network', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2},
+            {'filter_by': 'stations', 'stations': '501,502', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2},
+            {'filter_by': 'stations', 'stations': '[502, 501]', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2},
+            {'filter_by': 'cluster', 'cluster': self.cluster.id, 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2},
         ]
         for data in valid_form_data:
             form = CoincidenceDownloadForm(data)
@@ -70,8 +74,15 @@ class TestCoincidenceDownloadForm(TestCase):
 
     def test_clean_invalid(self):
         invalid_form_data = [
-            {'filter_by': 'cluster', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2},
+            ('cluster', 'invalid_choice', {'filter_by': 'cluster', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2}),
+            ('stations', 'required', {'filter_by': 'stations', 'stations': '', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2}),
+            ('stations', 'incorrect_entry', {'filter_by': 'stations', 'stations': '501;102', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2}),
+            ('stations', 'too_few', {'filter_by': 'stations', 'stations': '501', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2}),
+            ('stations', 'too_many', {'filter_by': 'stations', 'stations': (',501' * 32)[1:], 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2}),
+            ('stations', 'invalid_choices', {'filter_by': 'stations', 'stations': '501,501', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2}),
+            ('stations', 'invalid_choices', {'filter_by': 'stations', 'stations': '501,100001', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2}),
         ]
-        for data in invalid_form_data:
+        for field, error_code, data in invalid_form_data:
             form = CoincidenceDownloadForm(data)
             self.assertFalse(form.is_valid())
+            self.assertTrue(form.has_error(field, error_code), (field, error_code, data))
