7f3c8f86b73aa093e932f5e75ab381fa88c55333
Tom Kooij <tomkooij@tomkooij.nl>
2019-03-04 15:24:43 +0100

    Extract contact information from persons

--- scripts/contactlist.py
+++ scripts/contactlist.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import os
 import sys
 
@@ -13,6 +14,7 @@ django.setup()
 from publicdb.inforecords.models import *
 
 active_pcs = Pc.objects.exclude(type__slug='admin').filter(is_active=True)
+# all_pcs = Pc.objects.exclude(type__slug='admin').all()
 
 
 def clean_email(email):
@@ -22,17 +24,21 @@ def clean_email(email):
     return email.lower()
 
 
+def extract_email(contact_info):
+    email1, email2 = map(clean_email, (contact_info.email_work, contact_info.email_private))
+    return email1.lower(), email2.lower()
+
+
 contacts = set()
 for pc in active_pcs:
     sname = pc.station.name
     sn = str(pc.station.number)
-    contact_info = pc.station.contactinformation
-    email1, email2 = map(clean_email, (contact_info.email_work, contact_info.email_private))
-
-    email1 = email1.lower()
-    email2 = email2.lower()
+    if pc.station.contact is None:
+        continue
+    email1, email2 = extract_email(pc.station.contact.contactinformation)
     contacts.add(email1)
     contacts.add(email2)
+
     print ';'.join(item for item in (sname, sn, email1, email2))
 
 print '\n'.join(item for item in contacts)
