ebb972033a8a92f6556bacf139e1fdb5d06f3a6a
Arne de Laat <arne@delaat.net>
2019-03-04 14:53:53 +0100

    Simplify code to get contactlist.

--- scripts/contactlist.py
+++ scripts/contactlist.py
@@ -14,25 +14,15 @@ from publicdb.inforecords.models import *
 
 active_pcs = Pc.objects.exclude(type__slug='admin').filter(is_active=True)
 
-
-def clean_email(email):
-    """ Remove None and make all lowercase"""
-    if email is None:
-        return ''
-    return email.lower()
-
-
 contacts = set()
 for pc in active_pcs:
     sname = pc.station.name
     sn = str(pc.station.number)
     contact_info = pc.station.contactinformation
-    email1, email2 = map(clean_email, (contact_info.email_work, contact_info.email_private))
-
-    email1 = email1.lower()
-    email2 = email2.lower()
+    email1 = contact_info.email_work.lower() if contact_info.email_work else ''
+    email1 = contact_info.email_private.lower() if contact_info.email_private else ''
     contacts.add(email1)
     contacts.add(email2)
-    print ';'.join(item for item in (sname, sn, email1, email2))
+    print(';'.join([sname, sn, email1, email2]))
 
-print '\n'.join(item for item in contacts)
+print('\n'.join(contacts))
