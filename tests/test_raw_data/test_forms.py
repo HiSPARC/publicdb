0d0c0048b809e62eb9650aaa42f99b8bd4f02a35
Arne de Laat <arne@delaat.net>
2022-07-17 09:24:58 +0200

    Remove no longer required 'agree' form option

--- tests/test_raw_data/test_forms.py
+++ tests/test_raw_data/test_forms.py
@@ -24,10 +24,10 @@ class TestDataDownloadForm(TestCase):
 
     def test_clean_valid(self):
         valid_form_data = [
-            {'data_type': 'events', 'station_events': self.station.id, 'start': '2017-1-1', 'end': '2017-1-2', 'agree': True},
-            {'data_type': 'weather', 'station_weather': self.station.id, 'start': '2017-1-1', 'end': '2017-1-2', 'agree': True},
-            {'data_type': 'singles', 'station_singles': self.station.id, 'start': '2017-1-1', 'end': '2017-1-2', 'agree': True},
-            {'data_type': 'lightning', 'lightning_type': 0, 'start': '2014-10-1', 'end': '2014-11-4', 'agree': True},
+            {'data_type': 'events', 'station_events': self.station.id, 'start': '2017-1-1', 'end': '2017-1-2'},
+            {'data_type': 'weather', 'station_weather': self.station.id, 'start': '2017-1-1', 'end': '2017-1-2'},
+            {'data_type': 'singles', 'station_singles': self.station.id, 'start': '2017-1-1', 'end': '2017-1-2'},
+            {'data_type': 'lightning', 'lightning_type': 0, 'start': '2014-10-1', 'end': '2014-11-4'},
         ]
         for data in valid_form_data:
             form = DataDownloadForm(data)
@@ -35,10 +35,10 @@ class TestDataDownloadForm(TestCase):
 
     def test_clean_invalid(self):
         invalid_form_data = [
-            {'data_type': 'events', 'station_weather': self.station.id, 'start': '2017-1-1', 'end': '2017-1-2', 'agree': True},
-            {'data_type': 'weather', 'station_events': self.station.id, 'start': '2017-1-1', 'end': '2017-1-2', 'agree': True},
-            {'data_type': 'lightning', 'lightning_type': 10, 'start': '2014-10-1', 'end': '2014-11-4', 'agree': True},
-            {'data_type': 'events', 'station_events': self.station.id, 'agree': True},
+            {'data_type': 'events', 'station_weather': self.station.id, 'start': '2017-1-1', 'end': '2017-1-2'},
+            {'data_type': 'weather', 'station_events': self.station.id, 'start': '2017-1-1', 'end': '2017-1-2'},
+            {'data_type': 'lightning', 'lightning_type': 10, 'start': '2014-10-1', 'end': '2014-11-4'},
+            {'data_type': 'events', 'station_events': self.station.id},
         ]
         for data in invalid_form_data:
             form = DataDownloadForm(data)
@@ -63,10 +63,10 @@ class TestCoincidenceDownloadForm(TestCase):
 
     def test_clean_valid(self):
         valid_form_data = [
-            {'filter_by': 'network', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2, 'agree': True},
-            {'filter_by': 'stations', 'stations': '501,502', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2, 'agree': True},
-            {'filter_by': 'stations', 'stations': '[502, 501]', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2, 'agree': True},
-            {'filter_by': 'cluster', 'cluster': self.cluster.id, 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2, 'agree': True},
+            {'filter_by': 'network', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2},
+            {'filter_by': 'stations', 'stations': '501,502', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2},
+            {'filter_by': 'stations', 'stations': '[502, 501]', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2},
+            {'filter_by': 'cluster', 'cluster': self.cluster.id, 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2},
         ]
         for data in valid_form_data:
             form = CoincidenceDownloadForm(data)
@@ -74,13 +74,13 @@ class TestCoincidenceDownloadForm(TestCase):
 
     def test_clean_invalid(self):
         invalid_form_data = [
-            ('cluster', 'invalid_choice', {'filter_by': 'cluster', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2, 'agree': True}),
-            ('stations', 'required', {'filter_by': 'stations', 'stations': '', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2, 'agree': True}),
-            ('stations', 'incorrect_entry', {'filter_by': 'stations', 'stations': '501;102', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2, 'agree': True}),
-            ('stations', 'too_few', {'filter_by': 'stations', 'stations': '501', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2, 'agree': True}),
-            ('stations', 'too_many', {'filter_by': 'stations', 'stations': (',501' * 32)[1:], 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2, 'agree': True}),
-            ('stations', 'invalid_choices', {'filter_by': 'stations', 'stations': '501,501', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2, 'agree': True}),
-            ('stations', 'invalid_choices', {'filter_by': 'stations', 'stations': '501,100001', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2, 'agree': True}),
+            ('cluster', 'invalid_choice', {'filter_by': 'cluster', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2}),
+            ('stations', 'required', {'filter_by': 'stations', 'stations': '', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2}),
+            ('stations', 'incorrect_entry', {'filter_by': 'stations', 'stations': '501;102', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2}),
+            ('stations', 'too_few', {'filter_by': 'stations', 'stations': '501', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2}),
+            ('stations', 'too_many', {'filter_by': 'stations', 'stations': (',501' * 32)[1:], 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2}),
+            ('stations', 'invalid_choices', {'filter_by': 'stations', 'stations': '501,501', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2}),
+            ('stations', 'invalid_choices', {'filter_by': 'stations', 'stations': '501,100001', 'start': '2017-1-1', 'end': '2017-1-2', 'n': 2}),
         ]
         for field, error_code, data in invalid_form_data:
             form = CoincidenceDownloadForm(data)
