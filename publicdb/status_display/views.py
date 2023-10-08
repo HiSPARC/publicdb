e1d34a6865ece6838bd4f0cbb6ab7f73c7bdf6c5
Arne de Laat <arne@delaat.net>
2023-10-08 11:03:07 +0200

    Split map focus into separate function for easier logic

--- publicdb/status_display/views.py
+++ publicdb/status_display/views.py
@@ -148,25 +148,34 @@ def stations_by_name(request):
     return render(request, 'status_display/stations_by_name.html', {'stations': stations, 'statuscount': statuscount})
 
 
+def get_focus(country=None, cluster=None, subcluster=None):
+    if not country:
+        return Cluster.objects.all().values_list('name', flat=True)
+
+    country = get_object_or_404(Country, name=country)
+
+    if not cluster:
+        return country.clusters.values_list('name', flat=True)
+
+    cluster = get_object_or_404(country.clusters, name=cluster, parent=None)
+
+    if not subcluster:
+        focus = [cluster.name]
+        focus.extend(cluster.subclusters.values_list('name', flat=True))
+        return focus
+
+    if cluster.name == subcluster:
+        return [cluster.name]
+
+    subcluster = get_object_or_404(cluster.subclusters, name=subcluster)
+
+    return [subcluster.name]
+
+
 def stations_on_map(request, country=None, cluster=None, subcluster=None):
     """Show all stations from a subcluster on a map"""
 
-    if not country:
-        focus = Cluster.objects.all().values_list('name', flat=True)
-    else:
-        country = get_object_or_404(Country, name=country)
-        if not cluster:
-            focus = country.clusters.values_list('name', flat=True)
-        else:
-            cluster = get_object_or_404(country.clusters, name=cluster, parent=None)
-            if not subcluster:
-                focus = [cluster.name]
-                focus.extend(cluster.subclusters.values_list('name', flat=True))
-            else:
-                if cluster.name == subcluster:
-                    focus = [cluster.name]
-                else:
-                    focus = [get_object_or_404(cluster.subclusters, name=subcluster).name]
+    focus = get_focus(country, cluster, subcluster)
 
     data_stations = stations_with_data()
     station_status = DataStatus()
